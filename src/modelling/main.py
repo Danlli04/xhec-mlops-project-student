fc8c6f2bc23d281be0fd533f95d75ef659f8f485
Danli LIU <danli.liu.ye@gmail.com>
2023-10-23 17:36:12 +0200

    orchestration
    step 1: update env
    step 2:
     prefect config set PREFECT_API_URL=http://0.0.0.0:4200/api
    step 3: check db by
    sqlite3 --version
    step 4: prefect server start --host 0.0.0.0
    step 5: python main.py

--- src/modelling/main.py
+++ src/modelling/main.py
@@ -3,9 +3,13 @@
 import argparse
 import pandas as pd
 from pathlib import Path
+from typing import Optional
 from utils import save_pickle
-from preprocessing import compute_target, extract_x_y
-from training import train_model
+from training import train_model, predict, evaluate_model
+from prefect import flow, serve
+from loguru import logger
+from preprocessing import process_data
+from sklearn.model_selection import train_test_split
 
 # Define the relative path to the data folder
 DATA_FOLDER = Path("../../data")
@@ -17,25 +21,44 @@ trainset_path = DATA_FOLDER / "abalone.csv"
 def load_data(path: str):
     return pd.read_csv(path)
 
-def main(trainset_path: Path) -> None:
-    """Train a model using the data at the given path and save the model (pickle)."""
-    # Read data
-    df = load_data(path=trainset_path)
-    # Preprocess data
-    df = compute_target(df)
-    x, y, dv = extract_x_y(df)
+@flow(name="Train model")
+def train_model_workflow(
+    train_filepath: str,
+    artifacts_filepath: Optional[str] = None,
+) -> dict:
+    logger.info("Processing training data...")
+    X, y, dv = process_data(filepath=train_filepath, with_target=True)
+    logger.info("Training model...")
+    # Split the dataset into training and testing sets
+    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)  # using 20% of data for testing
 
     # Train model
-    model = train_model(x, y)
+    model = train_model(X_train, y_train)
+
+    # evaluate the model
+    logger.info("Making predictions and evaluating...")
+    y_pred = predict(X_test, model)
+    rmse = evaluate_model(y_test, y_pred)
 
     # Pickle model --> The model should be saved in pkl format the `src/web_service/local_objects` folder
     modelpath = MODELS_DIRPATH / "model.pkl"
     dvpath = MODELS_DIRPATH / "dv.pkl"
-    save_pickle(path=modelpath, obj=model)
-    save_pickle(path=dvpath, obj=dv)
+    if artifacts_filepath is not None:
+        logger.info(f"Saving artifacts to {artifacts_filepath}...")
+        save_pickle(path=modelpath, obj=model)
+        save_pickle(path=dvpath, obj=dv)
+
+    return {"model": model, "dv": dv, "rmse": rmse}
 
 if __name__ == "__main__":
-    parser = argparse.ArgumentParser(description="Train a model using the data at the given path.")
-    parser.add_argument("-t", "--trainset_path", type=str, default=str(trainset_path), help="Path to the training set")
-    args = parser.parse_args()
-    main(Path(args.trainset_path))
+    train_model_deployment = train_model_workflow.to_deployment(
+        name="Model training Deployment",
+        version="0.1.0",
+        tags=["training", "model"],
+        cron="0 0 * * 0",
+        parameters={
+            "train_filepath": trainset_path,
+            "artifacts_filepath": MODELS_DIRPATH,
+        },
+    )
+    serve(train_model_deployment)
--- src/modelling/preprocessing.py
+++ src/modelling/preprocessing.py
@@ -1,6 +1,11 @@
 from sklearn.feature_extraction import DictVectorizer
 import pandas as pd
-
+from prefect import flow, task
+from loguru import logger
+import scipy.sparse
+from typing import Tuple
+import numpy as np
+@task
 def compute_target(
     df: pd.DataFrame,
     rings_column: str = "Rings",
@@ -10,11 +15,12 @@ def compute_target(
     return df
 
 # Function to extract X and y using DictVectorizer
+@task
 def extract_x_y(
     df: pd.DataFrame,
     dv: DictVectorizer = None,
     with_target: bool = True,
-) -> dict:
+) -> Tuple[scipy.sparse.csr_matrix, np.ndarray, DictVectorizer]:
 
     dicts = df.drop(columns=["Age"] if "Age" in df.columns else []).to_dict(orient="records")
 
@@ -29,3 +35,24 @@ def extract_x_y(
         y = df["Age"].values
 
     return x, y, dv
+
+def load_data(path: str):
+    return pd.read_csv(path)
+
+@flow(name="Preprocess data")
+def process_data(filepath: str, dv=None, with_target: bool = True) -> scipy.sparse.csr_matrix:
+    """
+    Load data from a csv file
+    Turn features to sparce matrix
+    :return The sparce matrix, the target' values and the
+    dictvectorizer object if needed.
+    """
+    df = load_data(path=filepath)
+    if with_target:
+        logger.debug(f"{filepath} | Computing target...")
+        df1 = compute_target(df)
+        logger.debug(f"{filepath} | Extracting X and y...")
+        return extract_x_y(df1, dv=dv)
+    else:
+        logger.debug(f"{filepath} | Extracting X and y...")
+        return extract_x_y(df, dv=dv, with_target=with_target)
