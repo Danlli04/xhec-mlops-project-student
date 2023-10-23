35421a5e8995151f2270a1aae765eafdba72ca97
Danli LIU <116149776+Danlli04@users.noreply.github.com>
2023-10-23 18:25:42 +0200

    Update utils.py

--- src/modelling/utils.py
+++ src/modelling/utils.py
@@ -1,7 +1,9 @@
 # Use this module to code a `pickle_object` function. This will be useful to pickle the model (and encoder if need be).
 import pickle
 from typing import Any
+from prefect import flow, task
 
+@task(name="Save pickle")
 def save_pickle(path: str, obj: Any):
     with open(path, "wb") as f:
         pickle.dump(obj, f)
